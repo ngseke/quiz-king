35ae2f8777a9be6a53b2f9134f4d78bd6674f98f
Huang Xingqiao <a92304a92304@gmail.com>
2019-06-07 14:14:20 +0800

    可從標題畫面移動至關卡選擇畫面

--- QuizKing/ViewController.swift
+++ QuizKing/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func start(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "selectLevelSegue", sender: self)
+    }
+    
 }
 
